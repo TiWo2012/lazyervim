e2046806d4627a33649d4ea82b4d8b81827fbfea
Timo Wolf <timo.wolf@gmx.ch>
2025-11-23 12:33:00 +0100

    fixed the fugitive config to be even better to not require a lot of keypresses

--- lua/plugins/vimfugitive.lua
+++ lua/plugins/vimfugitive.lua
@@ -1,42 +1,23 @@
 return {
   {
     "tpope/vim-fugitive",
+    config = function()
+      vim.keymap.set("n", "<leader>gs", "<cmd>Git<CR>")
+    end,
 
     keys = {
       {
-        "<leader>gfs",
+        "<leader>Gs",
         function()
-          local width = math.floor(vim.o.columns * 0.9)
-          local height = math.floor(vim.o.lines * 0.9)
-          local row = math.floor((vim.o.lines - height) / 2)
-          local col = math.floor((vim.o.columns - width) / 2)
-
-          -- create a real buffer
-          local buf = vim.api.nvim_create_buf(true, false)
-
-          -- open the floating window
-          local win = vim.api.nvim_open_win(buf, true, {
-            relative = "editor",
-            width = width,
-            height = height,
-            row = row,
-            col = col,
-            style = "minimal",
-            border = "rounded",
-          })
-
-          -- run :Git inside the floating window
-          vim.api.nvim_win_call(win, function()
-            vim.cmd("Git")
-          end)
+          vim.cmd("Git")
         end,
-        desc = "Fugitive (Floating)",
+        desc = "fugitive menu",
       },
-      { "<leader>gfd", "<cmd>Gdiffsplit<cr>", desc = "Git Diff" },
-      { "<leader>gfb", "<cmd>Git blame<cr>", desc = "Git Blame" },
-      { "<leader>gfc", "<cmd>Git commit<cr>", desc = "Git Commit" },
-      { "<leader>gfp", "<cmd>Git push<cr>", desc = "Git Push" },
-      { "<leader>gfl", "<cmd>Git pull<cr>", desc = "Git Pull" },
+      { "<leader>Gd", "<cmd>Gdiffsplit<cr>", desc = "Git Diff" },
+      { "<leader>Gb", "<cmd>Git blame<cr>", desc = "Git Blame" },
+      { "<leader>Gc", "<cmd>Git commit<cr>", desc = "Git Commit" },
+      { "<leader>Gp", "<cmd>Git push<cr>", desc = "Git Push" },
+      { "<leader>GP", "<cmd>Git pull<cr>", desc = "Git Pull" },
     },
   },
 }
